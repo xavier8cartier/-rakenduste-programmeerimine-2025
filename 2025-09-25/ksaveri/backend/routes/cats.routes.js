b663af73a8422b952af7534c65c6fb87f62e7d3b
xavier8cartier <kpetrov1@tlu.ee>
2025-09-28 11:39:39 +0300

    (refactor): update routes for CRUD with validation

--- 2025-09-25/ksaveri/backend/routes/cats.routes.js
+++ 2025-09-25/ksaveri/backend/routes/cats.routes.js
@@ -4,14 +4,15 @@ const catsController = require("../controllers/cats.controller");
 const {
   catsRouteMiddleware,
   catsGetRouteMiddleware,
+  validateCat,
 } = require("../middlewares/cats.middlewares");
 
 router.use(catsRouteMiddleware);
 
 // /cats/ Get endpoint level middleware
 router.get("/", catsGetRouteMiddleware, catsController.read);
-router.post("/", catsController.create);
-router.put("/", catsController.update);
-router.delete("/", catsController.delete);
+router.post("/", validateCat, catsController.create);
+router.put("/:id", validateCat, catsController.update);
+router.delete("/:id", catsController.delete);
 
 module.exports = router;
